6aeb6f679dc2f115837001879384b1c63e511230
Schiefelbein <schieftg@l200210.cs.jmu.edu>
2016-12-13 15:02:51 -0500

    fixed up the code and added comments on my thoughts and thought process

--- scripts/detect_alvar.py
+++ scripts/detect_alvar.py
@@ -22,17 +22,21 @@ class Detector_Alvar(object):
     def __init__(self):
         rospy.init_node("detect_node")
 
+        self.finding = True
+        self.listOfVictims = []
         self.bridge = CvBridge()
+        self.map_msg = None
         self.imgCounter = 1
         self.found = False
         self.victim = Victim()
         self.thresh_hold = 1
-        self.curX = 2.86	 #hardcoded based off of current intital_pose.yaml
-	self.curY = -1.77	 #hardcoded based off of current intital_pose.yaml
-	self.listOfPoints = [(self.curX, self.curY)]
-	self.first = True
-	self.xpostive = 1
-	self.ypostive = 1
+        self.victim_hold = .1
+        self.curX = 2.86     #hardcoded based off of current intital_pose.yaml
+        self.curY = -1.77     #hardcoded based off of current intital_pose.yaml
+        self.listOfPoints = [(self.curX, self.curY)]
+        self.first = True
+        self.xpostive = 1
+        self.ypostive = 1
 
         self.ac = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         self.state_names = {}
@@ -49,12 +53,6 @@ class Detector_Alvar(object):
         rospy.Subscriber('map', OccupancyGrid, self.map_callback)
         rospy.Subscriber('/camera/rgb/image_raw',Image,self.icallback)
         rospy.Subscriber('/visualization_marker', Marker, self.detect_callback)
-        #pub.publish(self.victim)
-        #self.victim.id += 1
-
-        print "got here"        
-
-        self.map_msg = None
 
         while self.map_msg is None and not rospy.is_shutdown():
             rospy.loginfo("Waiting for map...")
@@ -63,31 +61,33 @@ class Detector_Alvar(object):
         self.map = map_utils.Map(self.map_msg)
 
         self.count = 0
-
-	print "found map"
-
         x_target = 0
         y_target = 0
         while not rospy.is_shutdown() and self.count < 3:
             #while not self.map.get_cell(x_target, y_target) == 0:
-
             x_target = random.uniform(-10,10)
             y_target = random.uniform(-10,10)
-	    print "try random point: ", x_target, ", ", y_target
+            print "try random point: ", x_target, ", ", y_target
             if self.map.get_cell(x_target, y_target) == 0:
                 if self.checkPoint(x_target,y_target):
-		    print "point was valid. going to", x_target,", ", y_target
+                    print "point was valid. going to", x_target,", ", y_target
                     self.count += 1
                     self.goto_point(x_target, y_target)
-            
-
-            #rospy.spin()
-
 
     def detect_callback(self, msg):
-        if msg.pose.position.z <= .9:
-            self.victim.point = msg.pose.position
+        if self.finding:
+            self.finding = False
+            if newVictim(msg):
+                self.ac.cancel_all_goals()
+                #get current x and y and pass them into addpoints
+                #self.goto_point(infront of victim with .9 distance away)
+                #self.goto_point(where robot was before going to infront of victim)
+            else:
+                self.finding = True
+
+        if msg.pose.position.z <= .9: ######stuff in here might need to be redone to fit current logic not sure as of now
             self.found = True
+            self.victim.point = msg.pose.position
             self.victim.id += 1
             rospy.loginfo("Victim Found")
             rospy.loginfo(self.victim)
@@ -101,9 +101,9 @@ class Detector_Alvar(object):
     def icallback(self,img):
         if self.imgCounter < 50 and self.found is True:
             try:
-                self.victim.image = img
-                self.pub.publish(self.victim)
                 self.found = False
+                self.victim.image = img
+                self.pub.publish(self.victim)               
                 cv_image = self.bridge.imgmsg_to_cv2(img,"rgb8")
                 #cv.SaveImage(str(imgCounter) + "image.jpg",cv_image)
                 cv2.imwrite(str(self.imgCounter) + "image.jpg",cv_image)
@@ -118,7 +118,6 @@ class Detector_Alvar(object):
 
         quat = tf.transformations.quaternion_from_euler(0, 0, theta_target)
         # Create a goal message ...
-        goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.get_rostime()
         goal.target_pose.pose.position.x = x_target
@@ -159,10 +158,26 @@ class Detector_Alvar(object):
         # Should be either "SUCCEEDED" or "ABORTED"
         state_name = self.state_names[self.ac.get_state()]
         rospy.loginfo("State      : {}".format(state_name))
+        ########## i believe there should be a addPoints call here but im not sure on the logic of the rest of the code and ill need to test the cancel call to make sure it doesnt add the points twice
 
-    def checkPoint(self,x,y): #break into 2 methods one for if its good to go second for filling in points
-        slope = 0
-        
+    def newVictim(self,msg):####################  the x will fail here but you can get the x from the msg.pose.x or whatever 
+        #will check if the position is a new victim
+        if len(self.listOfVictims) != 0:
+            for point in self.listOfVictims:
+                if (point[0] + self.victim_hold <= x - self.victim_hold or point[0] - self.victim_hold >= x + self.victim_hold) or (point[1] + self.victim_hold <= y - self.victim_hold or point[1] - self.victim_hold >= y + self.thresh_hold):
+                    newVictim = True
+                else:
+                    newVictim = False
+                    break
+        else:
+            return True
+        if newVictim:
+            #add new victim x and y to victim list
+            return True
+        else:
+            return False
+
+    def checkPoint(self,x,y):
         for point in self.listOfPoints:
             #if point is too close to an already-traversed point
             if (point[0] + self.thresh_hold <= x - self.thresh_hold or point[0] - self.thresh_hold >= x + self.thresh_hold) or (point[1] + self.thresh_hold <= y - self.thresh_hold or point[1] - self.thresh_hold >= y + self.thresh_hold):
@@ -171,27 +186,24 @@ class Detector_Alvar(object):
                 self.goodToGo = False
                 break
 
-        if self.goodToGo:
-            self.checkPosOrNeg(x,y)
-            if x-self.curX != 0:
-                slope = (y-self.curY)/(x-self.curX)
-                intercept = y - (slope * x)
-                self.listOfPoints.append((x+(1*self.xpositive),(x+(1*self.xpositive))*slope+intercept))
-                for val in range(1,int(math.floor(abs(x-self.curX))+1)):
-                    self.listOfPoints.append((self.curX+(val*self.xpositive),(self.curX+(val*self.xpositive))*slope+intercept))
+        return self.goodToGo
 
-            else:
-                self.listOfPoints.append((x,y+(1*self.ypositive)))
-                for val in range(1,int(math.floor(abs(y-self.curY))+1)):
-                    self.listOfPoints.append((self.curX,self.curY + (val*self.ypositive)))
+    def addPoints(self,x,y):
+        slope = 0
+        self.checkPosOrNeg(x,y)
+        if x-self.curX != 0:
+            slope = (y-self.curY)/(x-self.curX)
+            intercept = y - (slope * x)
+            self.listOfPoints.append((x+(1*self.xpositive),(x+(1*self.xpositive))*slope+intercept))
+            for val in range(1,int(math.floor(abs(x-self.curX))+1)):
+                self.listOfPoints.append((self.curX+(val*self.xpositive),(self.curX+(val*self.xpositive))*slope+intercept))
+        else:
+            self.listOfPoints.append((x,y+(1*self.ypositive)))
+            for val in range(1,int(math.floor(abs(y-self.curY))+1)):
+                self.listOfPoints.append((self.curX,self.curY + (val*self.ypositive)))
 
-            self.curX = x
-            self.curY = y
-	return self.goodToGo
-	
-                    
-#        print self.listOfPoints
-#        print "##########################################################"
+        self.curX = x
+        self.curY = y
 
     def checkPosOrNeg(self,x,y):
         if self.curX - x > 0:
